58daa00c392fa8e6917f361aba2d2dab1aa05239
Abdulrahman <abdulrahmanbadamasi123@gmail.com>
2022-05-28 12:14:40 +0100

    collision detection

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ var bricks = [];
 for(var c = 0; c < brickColumnCount; c++){
     bricks[c] = []
     for(var r = 0; r < brickRowCount; r++){
-        bricks[c][r] = {x: 0, y: 0};
+        bricks[c][r] = {x: 0, y: 0, ststus: 1};
     }
 }
 
@@ -55,7 +55,20 @@ function keyUpHandler(e) {
         leftPressed = false
     }
 }
-
+function colisionDetection(){
+    for(var c = 0; c < brickColumnCount; c++){
+        for(var r = 0; r < brickRowCount; r++){
+            var b = bricks[c][r]
+            if (b.ststus == 1 ) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickWidth ) {
+                    dy = -dy
+                    b.ststus = 0
+                }
+            }
+            bricks[c][r] = {x: 0, y: 0, ststus: 1};
+        }
+    }
+}
 
 function drawBall() {
 
@@ -70,6 +83,8 @@ function drawBall() {
 function drawBricks () {
     for(var c = 0; c < brickColumnCount; c++){
         for(var r = 0; r < brickRowCount; r++){
+            if (bricks[c][r].ststus == 1) {
+            
             var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft
             var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop
             bricks[c][r].x =  brickX;
@@ -79,6 +94,7 @@ function drawBricks () {
             ctx.fillStyle = '#0095dd'
             ctx.fill()
             ctx.closePath()
+            }
         }
     }
 }
@@ -97,6 +113,7 @@ function draw() {
     drawBricks()
     drawBall()
     drawPaddle()
+    colisionDetection()
     if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx
     }
